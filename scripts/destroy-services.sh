4b27a60c4cc6623a50849a334ab86a8ea9f06120
tommyb82 <thomasbunting@gmail.com>
2021-06-28 16:55:07 +0100

    Destroy cmds for router svc+app

--- scripts/destroy-services.sh
+++ scripts/destroy-services.sh
@@ -9,3 +9,11 @@ cf delete-service -f $(expand_var ${SERVICE_NAME_PG})
 # User-Provided Services
 #############
 cf delete-service -f $(expand_var $UPS_NAME)
+cf delete-service -f $(expand_var $UPS_NAME_ROUTE_SERVICE)
+
+#############
+# Router service app
+#############
+cf delete -f $(expand_var $APP_NAME_IP_ROUTER)
+
+

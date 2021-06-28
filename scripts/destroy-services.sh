a4a14c6403c58c0f93d714d668005911b610a5f8
tommyb82 <thomasbunting@gmail.com>
2021-06-28 17:08:14 +0100

    Remove router service bindings!

--- scripts/destroy-services.sh
+++ scripts/destroy-services.sh
@@ -9,6 +9,9 @@ cf delete-service -f $(expand_var ${SERVICE_NAME_PG})
 # User-Provided Services
 #############
 cf delete-service -f $(expand_var $UPS_NAME)
+
+# Uncomment / reinstate as required for each binding to the router service
+# cf unbind-service test-ip-router $(expand_var $UPS_NAME_ROUTE_SERVICE)
 cf delete-service -f $(expand_var $UPS_NAME_ROUTE_SERVICE)
 
 #############

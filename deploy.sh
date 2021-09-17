8ddbe3a23c4ba3cc685787afe62ebc7fea576b52
tommyb82 <thomasbunting@gmail.com>
2021-09-17 12:06:26 +0100

    Fix and tidy deployment vars

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@
 
 echo "Executing Terraform scripts in [$CF_ENVIRONMENT]"
 cd ./iac/environments/$CF_ENVIRONMENT/
-terraform init -backend-config="access_key=$ACCESS_KEY_ID" -backend-config="secret_key=$SECRET_ACCESS_KEY" -backend-config="bucket=$S3_STATE_BUCKET_NAME" -backend-config="dynamodb_table=$DDB_LOCK_TABLE_NAME"
+terraform init -backend-config="bucket=$S3_STATE_BUCKET_NAME" -backend-config="dynamodb_table=$DDB_LOCK_TABLE_NAME"
 terraform validate
 terraform apply -auto-approve -input=false -var="cf_username=$CF_USERNAME" -var="cf_password=$CF_PASSWORD"

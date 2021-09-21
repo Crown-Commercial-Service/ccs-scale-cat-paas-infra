b4d604dae50ad9b4039af597dbb0a38d698ed586
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2021-09-21 16:56:32 +0100

    Migrate IaC to Terraform (#5)
    * Migrate IaC to Terraform
    
    * Fix and tidy deployment vars
    
    * PR updates
    
    * Env specific instance+mem sizings
    
    * Added db create/delete timeouts

---
+++ iac/environments/nft/main.tf
@@ -0,0 +1,16 @@
+#########################################################
+# Environment: INT
+#
+# Deploy CaT resources
+#########################################################
+module "deploy-all" {
+  source                = "../../modules/configs/deploy-all"
+  space                 = "nft"
+  environment           = "nft"
+  cf_username           = var.cf_username
+  cf_password           = var.cf_password
+  syslog_drain_url      = "https://204572d5-f8ba-45f1-9e81-55e89762f616-ls.logit.io:12111"
+  postgres_service_plan = "medium-ha-11"
+  nginx_memory          = 2048
+  nginx_instances       = 3
+}

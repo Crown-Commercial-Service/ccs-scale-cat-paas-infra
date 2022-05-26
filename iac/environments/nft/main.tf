007d445312de923dec37d1aa134b7171c1ecf733
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2022-05-26 20:12:53 +0100

    Upgrade PG for TDB across remaining envs (#34)

--- iac/environments/nft/main.tf
+++ iac/environments/nft/main.tf
@@ -9,7 +9,7 @@ module "deploy-all" {
   environment           = "nft"
   cf_username           = var.cf_username
   cf_password           = var.cf_password
-  postgres_service_plan = "medium-ha-11"
+  postgres_service_plan = "medium-ha-13"
   nginx_memory          = 2048
   nginx_instances       = 3
   redis_service_plan    = "large-ha-6_x"

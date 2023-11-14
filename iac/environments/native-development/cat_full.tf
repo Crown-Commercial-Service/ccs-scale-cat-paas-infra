25263f00df36efdb90e02741a1e129e2a5727074
Chris Murton <cmurton@bettergov.co.uk>
2023-11-14 16:12:47 +0000

    GMBP-263: Update composition reference, variables and example tfvars

--- iac/environments/native-development/cat_full.tf
+++ iac/environments/native-development/cat_full.tf
@@ -1,12 +1,25 @@
 module "cat_full" {
   source = "../../compositions/cat-full"
 
-  aws_account_id           = var.aws_account_id
-  aws_region               = var.aws_region
-  docker_image_tags        = var.docker_image_tags
-  environment_is_ephemeral = var.environment_is_ephemeral
-  environment_name         = var.environment_name
-  resource_name_prefixes   = var.resource_name_prefixes
-  task_container_configs   = var.task_container_configs
-  vpc_cidr_block           = var.vpc_cidr_block
+  aws_account_id                   = var.aws_account_id
+  aws_region                       = var.aws_region
+  cat_api_ingress_cidr_safelist    = var.cat_api_ingress_cidr_safelist
+  cat_api_settings                 = var.cat_api_settings
+  docker_image_tags                = var.docker_image_tags
+  environment_is_ephemeral         = var.environment_is_ephemeral
+  environment_name                 = var.environment_name
+  hosted_zone                      = var.hosted_zone
+  rds_allocated_storage_gb         = var.rds_allocated_storage_gb
+  rds_backup_retention_period_days = var.rds_backup_retention_period_days
+  rds_db_instance_class            = var.rds_db_instance_class
+  rds_postgres_engine_version      = var.rds_postgres_engine_version
+  rds_skip_final_snapshot          = var.rds_skip_final_snapshot
+  resource_name_prefixes           = var.resource_name_prefixes
+  search_domain_engine_version     = var.search_domain_engine_version
+  search_domain_instance_count     = var.search_domain_instance_count
+  search_domain_volume_size_gib    = var.search_domain_volume_size_gib
+  service_subdomain_prefixes       = var.service_subdomain_prefixes
+  ssm_parameter_paths              = var.ssm_parameter_paths
+  task_container_configs           = var.task_container_configs
+  vpc_cidr_block                   = var.vpc_cidr_block
 }
--- iac/environments/native-development/variables.tf
+++ iac/environments/native-development/variables.tf
@@ -11,8 +11,28 @@ variable "aws_region" {
 variable "docker_image_tags" {
   type = object({
     buyer_ui_http = string,
+    cat_api_http  = string,
   })
   description = "Docker tag for deployment of each of the services from ECR"
+}
+
+variable "cat_api_ingress_cidr_safelist" {
+  type        = map(string)
+  description = "Map of CIDR blocks from which to accept requests for the public-facing Load Balancer for the CAT API, format {description: CIDR}"
+  validation {
+    condition     = length(var.cat_api_ingress_cidr_safelist) <= 20
+    error_message = "The cat_api_ingress_cidr_safelist can have a maximum of 20 entries."
+  }
+}
+
+variable "cat_api_settings" {
+  type = object({
+    dev_mode                   = bool,
+    eetime_enabled             = bool,
+    log_level                  = string,
+    resolve_buyer_users_by_sso = bool,
+  })
+  description = "Settings specific to the CAT API"
 }
 
 variable "environment_is_ephemeral" {
@@ -23,6 +43,43 @@ variable "environment_is_ephemeral" {
 variable "environment_name" {
   type        = string
   description = "Name for this environment, to distinguish it from other environments for this system / application."
+}
+
+variable "hosted_zone" {
+  type = object({
+    id   = string
+    name = string
+  })
+  description = "Properties of the Hosted Zone (which must be in the same AWS account as the resources) into which we will place alias and cert validation records"
+}
+
+variable "rds_allocated_storage_gb" {
+  type        = number
+  description = "Storage allocation in GiB"
+  default     = 10
+}
+
+variable "rds_backup_retention_period_days" {
+  type        = number
+  description = "Number of days for which to keep backups"
+  default     = 14
+}
+
+variable "rds_db_instance_class" {
+  type        = string
+  description = "Type of DB instance"
+  default     = "db.t3.small"
+}
+
+variable "rds_postgres_engine_version" {
+  type        = string
+  description = "Version number of db engine to use"
+  default     = "14.6"
+}
+
+variable "rds_skip_final_snapshot" {
+  type        = string
+  description = "Whether or not to skip the creation of a final snapshot of the db upon deletion"
 }
 
 # See naming convention doc:
@@ -36,10 +93,71 @@ variable "resource_name_prefixes" {
   description = "Prefix to apply to resources in AWS; options provided to satisfy divergent naming requirements across AWS"
 }
 
+variable "search_domain_engine_version" {
+  type        = string
+  description = "Version of OpenSearch engine to deploy"
+}
+
+variable "search_domain_instance_count" {
+  type        = number
+  description = "Number of instances in the OpenSearch cluster"
+}
+
+variable "search_domain_volume_size_gib" {
+  type        = number
+  description = "Size (in GiB) of the EBS volume to attach to each Opensearch instance"
+}
+
+variable "service_subdomain_prefixes" {
+  type = object({
+    cat_api = string,
+  })
+}
+
+variable "ssm_parameter_paths" {
+  type = object({
+    agreements-service-base-url               = string,
+    auth-server-jwk-set-uri                   = string,
+    conclave-wrapper-api-base-url             = string,
+    conclave-wrapper-api-key                  = string,
+    conclave-wrapper-identities-api-base-url  = string,
+    conclave-wrapper-identities-api-key       = string,
+    document-upload-service-api-key           = string,
+    document-upload-service-aws-access-key-id = string,
+    document-upload-service-aws-secret-key    = string,
+    document-upload-service-get-base-url      = string,
+    document-upload-service-s3-bucket         = string,
+    document-upload-service-upload-base-url   = string,
+    gov-uk-notify_api-key                     = string,
+    gov-uk-notify_invalid-duns-template-id    = string,
+    gov-uk-notify_target-email                = string,
+    gov-uk-notify_template-id                 = string,
+    jaggaer-base-url                          = string,
+    jaggaer-client-id                         = string,
+    jaggaer-client-secret                     = string,
+    jaggaer-itt-template-id                   = string,
+    jaggaer-project-template-id               = string,
+    jaggaer-self-service-id                   = string,
+    jaggaer-token-url                         = string,
+    oppertunities-s3-export-schedule          = string,
+    oppertunities-s3-export-ui-link           = string,
+    projects-to-opensearch-sync-schedule      = string,
+    rollbar-access-token                      = string,
+    rollbar-environment                       = string,
+  })
+  description = "Paths to the pre-existing SSM parameters necessary for CAS application functionality"
+}
+
 # See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/fargate-tasks-services.html#fargate-tasks-size
 variable "task_container_configs" {
   type = object({
     buyer_ui = object({
+      http_cpu     = number,
+      http_memory  = number,
+      total_cpu    = number,
+      total_memory = number,
+    }),
+    cat_api = object({
       http_cpu     = number,
       http_memory  = number,
       total_cpu    = number,

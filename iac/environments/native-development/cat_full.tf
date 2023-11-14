135e85254319aeead21426fd815a9bbb1e3bc96a
Mark Henwood <mcbhenwood@icloud.com>
2023-11-14 13:48:30 +0000

    Set up ECS cluster and skeleton of first service (Buyer UI)

--- iac/environments/native-development/cat_full.tf
+++ iac/environments/native-development/cat_full.tf
@@ -1,5 +1,12 @@
 module "cat_full" {
   source = "../../compositions/cat-full"
 
+  aws_account_id           = var.aws_account_id
+  aws_region               = var.aws_region
+  docker_image_tags        = var.docker_image_tags
   environment_is_ephemeral = var.environment_is_ephemeral
+  environment_name         = var.environment_name
+  resource_name_prefixes   = var.resource_name_prefixes
+  task_container_configs   = var.task_container_configs
+  vpc_cidr_block           = var.vpc_cidr_block
 }
--- iac/environments/native-development/variables.tf
+++ iac/environments/native-development/variables.tf
@@ -1,4 +1,54 @@
+variable "aws_account_id" {
+  type        = string
+  description = "AWS account into which to deploy resources"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "Region into which to deploy region-specific resources"
+}
+
+variable "docker_image_tags" {
+  type = object({
+    buyer_ui_http = string,
+  })
+  description = "Docker tag for deployment of each of the services from ECR"
+}
+
 variable "environment_is_ephemeral" {
   type        = bool
   description = "If true, indicates that the environment is expected to be destroyed from time to time - Allows for (e.g.) `force_destroy` on S3 buckets"
+}
+
+variable "environment_name" {
+  type        = string
+  description = "Name for this environment, to distinguish it from other environments for this system / application."
+}
+
+# See naming convention doc:
+#   https://crowncommercialservice.atlassian.net/wiki/spaces/GPaaS/pages/3561685032/AWS+3+Tier+Reference+Architecture
+variable "resource_name_prefixes" {
+  type = object({
+    normal        = string,
+    hyphens       = string,
+    hyphens_lower = string
+  })
+  description = "Prefix to apply to resources in AWS; options provided to satisfy divergent naming requirements across AWS"
+}
+
+# See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/fargate-tasks-services.html#fargate-tasks-size
+variable "task_container_configs" {
+  type = object({
+    buyer_ui = object({
+      http_cpu     = number,
+      http_memory  = number,
+      total_cpu    = number,
+      total_memory = number,
+    })
+  })
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR block to assign to the VPC"
 }

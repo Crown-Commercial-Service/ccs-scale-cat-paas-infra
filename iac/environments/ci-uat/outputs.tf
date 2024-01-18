60cb3432a46c3771ad2aebfcbdb0614f5dc78875
Andrew Hemming <drewhemm@users.noreply.github.com>
2024-01-18 14:10:11 +0000

    Update outputs.tf

--- iac/environments/ci-uat/outputs.tf
+++ iac/environments/ci-uat/outputs.tf
@@ -2,17 +2,6 @@ output "ecs_cluster_arn" {
   description = "ARN of the ECS Cluster which contains all the services"
   value       = module.cat_full.ecs_cluster_arn
 }
-
-output "ingestion_bucket_id" {
-  description = "Full name of the bucket which is to contain the ingestion objects"
-  value       = module.ingestion_bucket.bucket_id
-}
-
-output "ingestion_bucket_write_objects_policy_document_json" {
-  description = "JSON describing an IAM policy to allow writing of objects to the ingestion bucket"
-  value       = module.ingestion_bucket.write_objects_policy_document_json
-}
-
 output "public_buyer_ui_cert_validation_records_required" {
   description = "Details of the cert validation recrods required for the public-facing Buyer UI certificate"
   value       = module.cat_full.public_buyer_ui_cert_validation_records_required

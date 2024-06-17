260b96f225ee4a17ff29ec0620254e6111bc6089
ghill95 <george.hill@crowncommercial.gov.uk>
2024-06-17 10:38:38 +0100

    Add instance type to Opensearch domain definition

--- iac/compositions/cat-full/opensearch.tf
+++ iac/compositions/cat-full/opensearch.tf
@@ -9,6 +9,7 @@ module "search_domain" {
   ebs_volume_size_gib    = var.search_domain_volume_size_gib
   engine_version         = var.search_domain_engine_version
   instance_count         = var.search_domain_instance_count
+  instance_type          = var.search_domain_instance_type
   resource_name_prefixes = var.resource_name_prefixes
   subnet_ids             = module.vpc.subnets.database.ids
   vpc_id                 = module.vpc.vpc_id

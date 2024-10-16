d8a4c700902339215fc7372eff9d7b0a9307ef84
ghill95 <george.hill@crowncommercial.gov.uk>
2024-10-16 14:34:28 +0100

    COMP: Update outputs.tf

--- iac/compositions/cas-ui/outputs.tf
+++ iac/compositions/cas-ui/outputs.tf
@@ -1,4 +1,4 @@
-output "public_cas_ui_cert_validation_records_required" {
+/*output "public_cas_ui_cert_validation_records_required" {
   description = "Details of the cert validation records required for the public-facing cas UI certificate"
   value       = local.public_cas_ui_cert_validations
 }
@@ -11,4 +11,4 @@ output "public_cas_ui_cname_source" {
 output "public_cas_ui_cname_target" {
   description = "FQDN to which the public cas UI DNS CNAME should point"
   value       = aws_route53_record.cas_ui.fqdn
-}
+}*/

5486e4ba504c5561922b6b72020bf54e9cba6571
ghill95 <george.hill@crowncommercial.gov.uk>
2024-10-18 15:45:59 +0100

    Update subsequent environments

--- iac/environments/ci-sandbox/variables.tf
+++ iac/environments/ci-sandbox/variables.tf
@@ -33,6 +33,11 @@ variable "buyer_ui_public_fqdn" {
   description = "FQDN corresponding to the HOST header which will be present on all UI requests - This will be CNAMEd to the domain specified in the `hosted_zone_ui` variable"
 }
 
+variable "buyer_ui_redirect_r53_to_cas_ui" {
+  type        = bool
+  description = "Conditional to determine whether or not the R53 record for the Buyer UI should be redirected to CAS UI (as part of the CAS UI migration - defaults to false)"
+}
+
 variable "ca_cert_identifier" {
   type        = string
   description = "The identifier of the CA certificate for the DB instance."
@@ -46,6 +51,36 @@ variable "cas_buyer_ui_lb_waf_enabled" {
 variable "cas_cat_api_lb_waf_enabled" {
   type        = bool
   description = "Boolean value specifying whether or not the CAT API LB WAF Should be enabled"
+}
+
+variable "cas_ui_ingress_cidr_safelist" {
+  type        = map(string)
+  description = "Map of CIDR blocks from which to accept requests for the public-facing Load Balancer for the CAS UI, format {description: CIDR}"
+  validation {
+    condition     = length(var.cas_ui_ingress_cidr_safelist) <= 20
+    error_message = "The cas_ui_ingress_cidr_safelist can have a maximum of 20 entries."
+  }
+}
+
+variable "cas_ui_lb_waf_enabled" {
+  type        = bool
+  description = "Boolean value specifying whether or not the CAS UI LB WAF Should be enabled"
+}
+
+variable "cas_ui_public_cert_attempt_validation" {
+  type        = bool
+  default     = true
+  description = "If set to `false`, prevents Terraform from trying to validate the cert ownership - This will the the setting required when you first apply Terraform, to enable the process to finish cleanly. Once CNAME records have been created according to the output `public_cas_ui_cert_validation_records_required`, you can reset this variable to `true` and re-apply."
+}
+
+variable "cas_ui_public_fqdn" {
+  type        = string
+  description = "FQDN corresponding to the HOST header which will be present on all UI requests - This will be CNAMEd to the domain specified in the `hosted_zone_ui` variable"
+}
+
+variable "cas_web_acl_name" {
+  type        = string
+  description = "The name of the Web ACL (to be associated with enabled Load Balancers)"
 }
 
 variable "cas_web_acl_arn" {
@@ -144,6 +179,14 @@ variable "hosted_zone_api" {
     name = string
   })
   description = "Properties of the Hosted Zone (which must be in the same AWS account as the resources) into which we will place alias and cert validation records for the API"
+}
+
+variable "hosted_zone_cas_ui" {
+  type = object({
+    id   = string
+    name = string
+  })
+  description = "Properties of the Hosted Zone (which must be in the same AWS account as the resources) into which we will place alias and cert validation records for the UI"
 }
 
 variable "hosted_zone_ui" {

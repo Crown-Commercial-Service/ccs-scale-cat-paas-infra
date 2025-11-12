735cf00dcafa11c596662e6f0bea04c65971338e
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2025-11-12 11:48:44 +0000

    initial commit for the qa service

---
+++ iac/compositions/cas-qa-service/outputs.tf
@@ -0,0 +1,19 @@
+output "cas_ui_base_cert_validation_records_required" {
+  description = "Details of the cert validation records required for the [env]-cas-QA certificate"
+  value       = local.cas_qa_base_cert_validations
+}
+
+output "public_cas_ui_cert_validation_records_required" {
+  description = "Details of the cert validation records required for the public-facing cas QA certificate"
+  value       = local.public_cas_qa_cert_validations
+}
+
+output "public_cas_ui_cname_source" {
+  description = "DNS record to CNAME to the cas QA in this stack"
+  value       = var.cas_qa_public_fqdn
+}
+
+output "public_cas_ui_cname_target" {
+  description = "FQDN to which the public cas QA DNS CNAME should point"
+  value       = aws_route53_record.cas_qa.fqdn
+}

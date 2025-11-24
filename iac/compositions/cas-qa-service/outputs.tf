130e0eca8269c528b589ea6294c170921f272324
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2025-11-24 16:09:28 +0000

    updates

--- iac/compositions/cas-qa-service/outputs.tf
+++ iac/compositions/cas-qa-service/outputs.tf
@@ -1,8 +1,3 @@
-output "cas_qa_base_cert_validation_records_required" {
-  description = "Details of the cert validation records required for the [env]-cas-QA certificate"
-  value       = local.cas_qa_base_cert_validations
-}
-
 output "public_cas_qa_cert_validation_records_required" {
   description = "Details of the cert validation records required for the public-facing cas QA certificate"
   value       = local.public_cas_qa_cert_validations

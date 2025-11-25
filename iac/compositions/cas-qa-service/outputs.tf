051b6c51d913ee6e13e49ffe206f0ac0a60d549c
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2025-11-25 13:29:19 +0000

    updates again

--- iac/compositions/cas-qa-service/outputs.tf
+++ iac/compositions/cas-qa-service/outputs.tf
@@ -1,8 +1,3 @@
-output "public_cas_qa_cert_validation_records_required" {
-  description = "Details of the cert validation records required for the public-facing cas QA certificate"
-  value       = local.public_cas_qa_cert_validations
-}
-
 output "public_cas_qa_cname_source" {
   description = "DNS record to CNAME to the cas QA in this stack"
   value       = var.cas_qa_public_fqdn

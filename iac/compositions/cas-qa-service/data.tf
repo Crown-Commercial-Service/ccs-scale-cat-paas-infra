e07322d4f6352dd45a03aa2bcdc1a0619747e336
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2025-12-03 13:13:38 +0000

    updates

--- iac/compositions/cas-qa-service/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_ssm_parameter" "api_key" {
-  name = "/cat/${var.environment}/question-and-answer-service-api-key"
-}
-
-data "aws_ssm_parameter" "base_url" {
-  name = "/cat/${var.environment}/question-and-answer-service-base-url"
-}

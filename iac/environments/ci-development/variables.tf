718fb5d23eeb0dc28595ec5e17498e6b4ced3c21
Anantha Subramanian <bitsy@Mac-2.powerhub>
2026-06-02 16:17:10 +0100

    fix lb flag set default to false

--- iac/environments/ci-development/variables.tf
+++ iac/environments/ci-development/variables.tf
@@ -199,6 +199,7 @@ variable "enable_lb_access_logs" {
 variable "enable_lb_connection_logs" {
   type        = bool
   description = "If 1, enables ALB connection logging"
+  default     = false
 }
 
 variable "environment" {
